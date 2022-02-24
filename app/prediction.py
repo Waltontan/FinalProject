4bee6c642903c7f154f3a57139093eb09b9723a0
WaltonTan <walton.tan@hotmail.com>
2022-02-24 20:48:20 +0800

    prediction.py updated with new model

--- app/prediction.py
+++ app/prediction.py
@@ -3,40 +3,46 @@ import joblib
 
 def predict(user_inputs):
     # load model binaries 
-    model = joblib.load("static/py/model.sav")
+    model = joblib.load("static/py/model_trained.h5")
     encoder = joblib.load("static/py/encoder.sav")
     X_scaler = joblib.load("static/py/x_scaler.sav")
     y_scaler  = joblib.load("static/py/y_scaler.sav")
 
     # get the user input data 
-    pressure = user_inputs["pressure"]
-    humidity = user_inputs["humidity"]
-    city_name = user_inputs["city_name"]
-    
-    # store city names into a df 
-    city_input_df = pd.DataFrame({
-        "city_name": [city_name]
-    })
-
-    # use encoder to transform the city df 
-    X_transformed = encoder.transform(city_input_df)
-    city_df = pd.DataFrame(columns=[*encoder.categories_], data=X_transformed.toarray())
+    fixed_acidity = user_inputs["fixed acidity"]
+    volatile_acidity = user_inputs["volatile acidity"]
+    citric_acid = user_inputs["citric acid"]
+    residual_sugar = user_inputs["residual sugar"]
+    chlorides = user_inputs["chlorides"]
+    free_sulfur_dioxide = user_inputs["free sulfur dioxide"]
+    total_sulfur_dioxide = user_inputs["total sulfur dioxide"]
+    density = user_inputs["density"]
+    pH = user_inputs["pH"]
+    sulphates = user_inputs["sulphates"]
+    alcohol = user_inputs["alcohol"]
     
     # store pressure and humidty into df 
     input_df = pd.DataFrame({
-        "pressure": [pressure],
-        "humidity": [humidity]
+        "fixed_acidity": [fixed_acidity],
+        "volatile_acidity": [volatile_acidity],
+        "citric_acid": [citric_acid],
+        "residual_sugar": [residual_sugar],
+        "chlorides": [chlorides],
+        "free_sulfur_dioxide": [free_sulfur_dioxide],
+        "total_sulfur_dioxide": [total_sulfur_dioxide],
+        "density": [density],
+        "pH": [pH],
+        "sulphates": [sulphates],
+        "alcohol": [alcohol]
     })
 
-    # combine both df's using indexes 
-    df = input_df.merge(city_df, left_index=True, right_index=True)
-
     # scale the X input df 
-    X_scaled = X_scaler.transform(df)
+    X_scaled = X_scaler.transform(input_df)
 
     # obtain prediction (y) 
-    prediction_scaled = model.predict(X_scaled)
+    encoded_predictions = model.predict_classes(X_scaled[:5])
+    
     
     # scale prediction to human readable terms i.e. celcius 
-    prediction = y_scaler.inverse_transform(prediction_scaled)
-    return prediction[0][0]
+    prediction_labels = encoder.inverse_transform(encoded_predictions)
+    return prediction_labels[0]
